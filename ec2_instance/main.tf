902bedbccaafc0c01b2f63455d00eb90fe4b6c8c
Sahil Pachlore <71082807+SahilPachlore@users.noreply.github.com>
2023-04-15 11:42:39 +0530

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -7,16 +7,16 @@ terraform {
   }
 }
 provider "aws" {
-    region = "ap-northeast-1"
+    region = "eu-west-2"
     access_key = " "
     secret_key = " "
 }
 resource "aws_instance" "linux1_server" {
-    ami = var.ami
-    instance_type = var.instance_type
-    # count = "5"
+    ami = "ami-0cd8ad123effa531a"
+    instance_type = "t2.micro"
+   
   
 }
-output " public_ip" {
-    value = "aws_instance.linux1_server.public_ip "
+output "public_ip" {
+    value = aws_instance.linux1_server.public_ip 
 } 
