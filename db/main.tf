016f9be6585f05dc80b08a8dc7488d6771aee0e3
Sahil Pachlore <71082807+SahilPachlore@users.noreply.github.com>
2023-04-15 12:44:49 +0530

    Update main.tf

--- db/main.tf
+++ db/main.tf
@@ -59,3 +59,35 @@ resource "aws_db_instance" "rds" {
     Name = "rds"
   }
 }
+///----------------------------------------------------------///
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIATMLKWVXWDHXOPWHQ"
+    secret_key = "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"
+}
+
+resource "aws_db_instance" "rds" {
+  identifier           = "rdsinstancel"
+  allocated_storage    = 35
+  engine               = "mariadb"
+  engine_version       = "10.4.21"
+  instance_class       = "db.t3.micro"
+  username             = "admin"
+  password             = "admin123" 
+  publicly_accessible  = true
+  
+  storage_type = "gp2"
+
+  tags =  {
+    Name = "rdsinstancel"
+  }
+}
