3503648e13a82583fc54bc7e4d02055574cdf0e4
Sahil Pachlore <71082807+SahilPachlore@users.noreply.github.com>
2023-04-16 23:57:52 +0530

    Update main1.tf

--- ec2_instance/main1.tf
+++ ec2_instance/main1.tf
@@ -28,3 +28,27 @@ resource "aws_subnet" "main" {
   
 }
 
+///////////////////--------------------------------///////////////////////////////
+
+terraform {
+  required_providers {
+    aws={
+      source = "hashicorp/aws"
+      
+    }
+  }
+}
+provider "aws" {
+    region = "ap-northeast-1"
+    access_key = " "
+    secret_key = " "
+}
+resource "aws_instance" "linux_server" {
+    ami = "ami-0cc8b0ca07fee465f"
+    instance_type = "t2.micro"
+    # count = "5"
+  
+}
+output "public_ip" {
+    value = "aws_instance.linux_server.public_ip "
+} 
